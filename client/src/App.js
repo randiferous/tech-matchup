89f2d8a36c4fd93895795784603ca9a9a07f3123
lotus <daniel.younghwan.lee@gmail.com>
2022-09-30 15:29:31 -0400

    figuring out vote bug

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home';
-// import Matchup from './pages/Matchup';
-// import Vote from './pages/Vote';
-// import NotFound from './pages/NotFound';
+import Matchup from './pages/Matchup';
+import Vote from './pages/Vote';
+import NotFound from './pages/NotFound';
 
 const client = new ApolloClient({
   uri: 'graphql',
@@ -18,8 +18,20 @@ function App() {
         <div className="flex-column justify-center align-center min-100-vh bg-primary">
           <Routes>
             <Route
-            path="/"
-            element={<Home/>}
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/matchup"
+              element={<Matchup />}
+            />
+            <Route
+              path="/matchup/:id"
+              element={<Vote />}
+            />
+             <Route
+              path="*"
+              element={<NotFound />}
             />
           </Routes>
         </div>
